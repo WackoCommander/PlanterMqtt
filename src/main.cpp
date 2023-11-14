34da23d16971914ef6fd9ffbb6e1246d6032a10a
WackoCommander <84298309+WackoCommander@users.noreply.github.com>
2023-11-14 20:53:38 +1100

    Successful connection to the internet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <Arduino.h>
+// #include <PubSubClient.h>
+// #include <ArduinoJson.h>
+// #include <WiFiNINA.h>
+// #include <ESP8266WiFi.h>
+#include <WiFi.h>
+// #include <WiFiNINA_Generic.h>
+const char* WIFI_SSID = "Hydrapeak";
+const char* WIFI_PASSWRD = "Rectal5-Patchy-Everybody";
+
+
+
+// WiFiClient wifi_client;
+// PubSubClient client(wifi_client);
 
 void setup() {
-
+    Serial.begin(9600);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWRD);
+    Serial.print("Connecting to Wifi...");
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.print("Connected...");
 }
 
 void loop() {
