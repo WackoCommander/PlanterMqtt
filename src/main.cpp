caa53777aebe1209d57f2f6bff9de0f7e8d157c8
WackoCommander <84298309+WackoCommander@users.noreply.github.com>
2023-11-16 20:17:09 +1100

    Connected to Mqtt Server

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
 #include <Arduino.h>
-// #include <PubSubClient.h>
-// #include <ArduinoJson.h>
-// #include <WiFiNINA.h>
-// #include <ESP8266WiFi.h>
-#include <WiFi.h>
-// #include <WiFiNINA_Generic.h>
-const char* WIFI_SSID = "Hydrapeak";
+#include <PubSubClient.h>
+#include <WiFiS3.h>
+#include "WifiCreds.h"
+#include "MqttCreds.h"
 
-
-
-// WiFiClient wifi_client;
-// PubSubClient client(wifi_client);
+WiFiClient wifi_client;
+PubSubClient client(wifi_client);
 
 void setup() {
     Serial.begin(9600);
     WiFi.begin(WIFI_SSID, WIFI_PASSWRD);
-    Serial.print("Connecting to Wifi...");
+    Serial.println("Connecting to Wifi...");
     if (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
     }
-    Serial.print("Connected...");
+    Serial.println("Connected...");
+    client.setServer(MQTT_SERVER, MQTT_PORT);
+    Serial.println("Connecting to Mqtt server...");
+
+  while (!client.connected()) {
+    Serial.print(".");
+
+    if (client.connect("test", MQTT_USER, MQTT_PASSWRD)) {
+
+      Serial.println("Connected to MQTT");
+    } else {
+      Serial.println("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
 
 void loop() {
